53bba4fbbc5f32a5c4469089f3a43e1bbd36d885
toshihisa handa <toshihisa.handa@caters.co.jp>
2021-04-30 15:09:31 +0900

    追加機能を実装

--- src/Model/Table/ArticlesTable.php
+++ src/Model/Table/ArticlesTable.php
@@ -3,6 +3,8 @@
 namespace App\Model\Table;
 
 use Cake\ORM\Table;
+// Text クラス
+use Cake\Utility\Text;
 
 class ArticlesTable extends Table
 {
@@ -10,5 +12,16 @@ class ArticlesTable extends Table
     {
        $this->addBehavior('Timestamp');//このテーブルの created や modified カラムを自動的に更新する
     }
+
+    public function beforeSave($event, $entity, $options)
+    {
+        if ($entity->isNew() && !$entity->slug) {
+            $sluggedTitle = Text::slug($entity->title);
+            // スラグをスキーマで定義されている最大長に調整
+            $entity->slug = substr($sluggedTitle, 0, 191);
+        }
+    }
+
 }
+
 
